4ac1f7412e7146f651eb2538287a42e31e53fad5
Maserati <maserati@localhost.localdomain>
2025-04-16 14:39:27 -0400

    engine Google atualizacao

--- engineGoogle.py
+++ engineGoogle.py
@@ -7,18 +7,18 @@ import useragent
 def request(term, results, lang, start_index, proxies, timeout, safe, ssl_verify, region):
     ua = useragent
     r = requests.get(
-        url="https://www.google.com/search",
+        url='https://www.google.com/search',
         headers={
-            "User-Agent": ua.get_useragent(),
-            "Accept": "*/*"
+            'User-Agent': ua.get_useragent(),
+            'Accept': '*/*'
         },
         params={
-            "q": term,
-            "num": results + 2,
-            "hl": lang,
-            "start": start_index,
-            "safe": safe,
-            "gl": region,
+            'q': term,
+            'num': results + 2,
+            'hl': lang,
+            'start': start_index,
+            'safe': safe,
+            'gl': region,
         },
         proxies=proxies,
         timeout=timeout,
@@ -38,39 +38,26 @@ class SearchResult:
         self.description = description
 
     def __repr__(self):
-        return f"SearchResult(url={self.url}, title={self.title}, description={self.description})"
-
-'''
-param = 'cat'
-results_num = 10
-lang = 'en'
-start_index = 0 # indica em qual indice deve comecar
-proxies = None # [https://5.111.250.18:856, http://95.111.250.18:1456]
-timeout = 5
-details = False
-safe = 'active' # [active] [off]
-ssl_verify = False # [True] [False] [None]
-region = None
-unique = True # Nao mantem resultados repetidos
-'''
+        return f'SearchResult(url={self.url}, title={self.title}, description={self.description})'
 
 def search(param, results_num=10, lang='en', start_index=0, proxies=None, timeout=5, details=False, safe='active', ssl_verify=None, region=None, unique=True):
     
-    proxies = {"https": proxies, "http": proxies} if proxies and (proxies.startswith("https") or proxies.startswith("http")) else None
+    proxies = {'https': proxies, 'http': proxies} if proxies and (proxies.startswith('https') or proxies.startswith('http')) else None
 
     fetched_links = set()
     current_results_num = 0
 
     while current_results_num < results_num:
 
-        response = request(param, results_num, lang, start_index, proxies, timeout, safe, ssl_verify, region)
-        soup = BeautifulSoup(response.text, "html.parser")
-        result_block = soup.find_all("div", class_="ezO2md")
+        response = request(param, results_num, lang, start_index, 
+                            proxies, timeout, safe, ssl_verify, region)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        result_block = soup.find_all('div', class_='ezO2md')
         
         for result in result_block:
-            link_tag = result.find("a", href=True)
-            title_tag = link_tag.find("span", class_="CVA68e") if link_tag else None
-            description_tag = result.find("span", class_="FrIlee")
+            link_tag = result.find('a', href=True)
+            title_tag = link_tag.find('span', class_='CVA68e') if link_tag else None
+            description_tag = result.find('span', class_='FrIlee')
 
             metadata = {
                 'url': unquote(link_tag['href'].split('&')[0].replace('/url?q=', '')) if link_tag else 'Unknown',
@@ -78,7 +65,7 @@ def search(param, results_num=10, lang='en', start_index=0, proxies=None, timeou
                 'description': description_tag.text.strip() if description_tag and description_tag.text else 'Unknown'
             }
 
-            if metadata['url'] == 'Unknown' or not metadata['url'].startswith('http') or not metadata['url'].startswith('https'):
+            if metadata['url'] == 'Unknown' or not metadata['url'].startswith(('http', 'https')):
                 continue
 
             if unique and metadata['url'] in fetched_links:
