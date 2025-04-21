aa13436c6b77a5024905de166f17d23aa7f58719
Maserati <maserati@localhost.localdomain>
2025-04-20 21:12:14 -0400

    panels

---
+++ panels.py
@@ -0,0 +1,26 @@
+def menu_panel():
+    print('MENU PRINCIPAL')
+    print('''
+[1] - Dork
+[2] - Set
+[3] - About_Me
+[0] - Sair
+''')
+
+def dork_panel():
+    print('DORK')
+    print('''
+[1] - Google
+[2] - Bing
+[0] - Sair
+''')
+
+def about_me():
+    print('ABOUT_ME')
+    print('The macaws above my head dont let me think.')
+
+    print('#SOCIAL')
+    print('Github: https://github.com/bluurw')
+    print('Telegram: https://t.me/mk_Directory')
+    print()
+    input('...PRESS ANY KEY...')
